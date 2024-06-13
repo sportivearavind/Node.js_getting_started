2a6f02ceb59f9f8b27e4be05e0e6ce230c5b30ff
Aravind <aravind16101800@gmail.com>
2024-06-13 21:28:39 +0530

    feat: getting started with Event Emitter

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const {format} = require('date-fns')
-
-const {v4: uuid} = require('uuid') // every time a new unique id is created
-
-console.log(format(new Date(), 'ddMMyyyy\tHH:mm:ss'));
-
-console.log(uuid()); 
+const logEvents = require('./logEvents')
