fb70c3baaeb2c6e27500fdd1b768bbcfc24677e8
Aravind <aravind16101800@gmail.com>
2024-06-13 18:18:24 +0530

    feat: reading file using async and await

--- server.js
+++ server.js
@@ -1,53 +1,60 @@
-const fs = require("fs");
-const path = require("path");
-const fsPromises = require("fs").promises;
 
-// reading a file
-fs.readFile(path.join(__dirname, "files", "start.txt"), "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const path = require("path");
+const fsPromises = require("fs").promises; // importing filesystem promises
+
+const fileOps = async () => {
+    try{
+        const data = await fsPromises.readFile(path.join(__dirname,'files','start.txt'),'utf8')
+        console.log(data);
+    }catch(err){
+        console.error(err);
+    }
+} 
+
+fileOps();
+
+
 
 // writing a file
-fs.writeFile(
-  path.join(__dirname, "files", "end.txt"),
-  "End the program",
-  (err) => {
-    if (err) throw err;
-    console.log("Write complete");
-  }
-);
+// fs.writeFile(
+//   path.join(__dirname, "files", "end.txt"),
+//   "End the program",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Write complete");
+//   }
+// );
 
 // update a file
-fs.appendFile(
-  path.join(__dirname, "files", "end.txt"),
-  "End the program, See u next time",
-  (err) => {
-    if (err) throw err;
-    console.log("append complete");
-  }
-);
+// fs.appendFile(
+//   path.join(__dirname, "files", "end.txt"),
+//   "End the program, See u next time",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("append complete");
+//   }
+// );
 
 // The best practice is appending inside the write file
 // Buy this also called callback hell
 // writing a file
-fs.writeFile(
-  path.join(__dirname, "files", "end.txt"),
-  "End the program",
-  (err) => {
-    if (err) throw err;
-    console.log("Write complete");
-    // adding the appendFile as callback to the writeFile
-    fs.appendFile(
-      path.join(__dirname, "files", "end.txt"),
-      "End the program, See u next time",
-      (err) => {
-        if (err) throw err;
-        console.log("append complete");
-      }
-    );
-  }
-);
+// fs.writeFile(
+//   path.join(__dirname, "files", "end.txt"),
+//   "End the program",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Write complete");
+//     // adding the appendFile as callback to the writeFile
+//     fs.appendFile(
+//       path.join(__dirname, "files", "end.txt"),
+//       "End the program, See u next time",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("append complete");
+//       }
+//     );
+//   }
+// );
 
 // Note: we can also create file using appendFile
 
