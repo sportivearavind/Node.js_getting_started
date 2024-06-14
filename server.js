242ffad76b79b676dc19e21ee93e086f7b6fde60
Aravind <aravind16101800@gmail.com>
2024-06-14 19:29:52 +0530

    feat: reading our index.html file in server.js file

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http')
-const PORT = 3001
+const PORT = 3002
+const fs = require('fs')
 // creating server
 const server = http.createServer((req,res) => {
-    res.write("Hi Node server")
-    res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html', (error, data) => {
+        if(error){
+            res.writeHead(404)
+            res.write('Page not found')
+        }else{
+            res.write(data)
+        }
+
+        res.end()
+    })
 })
 
 // listening to server in port
